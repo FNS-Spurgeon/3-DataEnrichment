e6807e9844dc0da8fb169057a4a7d1b438ac9f66
leblance <elina.leblanc@live.fr>
2026-09-02 17:37:18 +0200

    Add enriched CSV file for Appendix A

--- scripts/languageDetection.py
+++ scripts/languageDetection.py
@@ -2,7 +2,7 @@ import pandas as pd
 from langid import langid
 
 list_lang = []
-csv_file_path = "vol2-1826-1850-allusions.csv"
+csv_file_path = "vol3-appendixA-allusions.csv"
 df = pd.read_csv(csv_file_path)
 
 for i in df["Quotation"]:
@@ -13,4 +13,4 @@ for i in df["Quotation"]:
         list_lang.append("N/A")
 
 df["language"] = list_lang
-df.to_csv("vol2-1801-1825-allusions-enriched.csv", index=False)
+df.to_csv("vol3-appendixA-allusions-enriched.csv", index=False)
